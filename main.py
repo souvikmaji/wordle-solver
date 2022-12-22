c00c515dbd5706f5d5fcb455972961dd9dc0fad3
souvikmaji <souvikmaji94@gmail.com>
2022-12-22 17:08:43 +0530

    basic version with containing and not containing words

--- main.py
+++ main.py
@@ -1,55 +1,44 @@
 import pickle
-import string
-from typing import Dict
 
 import typer
 
 
-def get_words(n=6):
+def get_words(n=6) -> dict[int, list[str]]:
     """open pickle file based on n and return list of words"""
-    file_name = f'words_{n}.pickle'
+    file_name = f'words/{n}.pickle'
     with open(file_name, 'rb') as f:
         words = pickle.load(f)
     return words
 
 
-def get_letters():
-    pass
-
-
-def find_words(words, containing, not_containing, specific_postions):
-    return words
-
-
 def print_words(words: list):
-    for word in words:
+    for word in words[:10]:
         print(word)
 
 
-def wordle(n: int = 6, containing: str = {string.ascii_lowercase},
-           not_containing: str = None):  # , specific_postions: Dict[int, str] = None):
-    """from a list of words find words containing specified letters"""
-    # if specific_positions is None:
-    #     specific_positions = {}
+def wordle(words: list[str], containing: str, not_containing: str):  # , specific_postions: Dict[int, str] = None):
 
-    if not_containing is None:
-        not_containing = []
-    else:
-        not_containing = not_containing.split('')
+    if containing and not_containing:
+        words = [word for word in words if all(letter in word for letter in containing) and
+                 all(letter not in word for letter in not_containing)]
 
-    words = get_words(n)
-    words = find_words(words, containing, not_containing)  # , specific_positions)
+    return words
 
 
 def main(n: int = 6):
-    containing_letters = typer.prompt("Enter letters to contain: ")
-    not_containing_letters = typer.prompt("Enter letters to not contain: ")
-
     words = get_words(n=6)
 
-    while len(words) > 1:
-        words = wordle(words, n, containing_letters, not_containing_letters)
+    containing = ''
+    not_containing = ''
+
+    while len(words) != 1:
+        words = wordle(words, containing, not_containing)
         print_words(words)
+
+        containing = typer.prompt("Enter letters to contain: ", default=containing)
+        not_containing = typer.prompt("Enter letters to not contain: ", default=not_containing)
+
+    print(words)
 
 
 if __name__ == '__main__':
